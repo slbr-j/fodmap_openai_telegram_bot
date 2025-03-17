84006543e5a1f9cccba553f24a5de663e0613734
slbr-j <104697639+slbr-j@users.noreply.github.com>
2025-03-17 17:14:21 +0100

    Update keyboards.py

--- app/keyboards.py
+++ app/keyboards.py
@@ -1,9 +1,21 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
-def get_main_keyboard():
+def get_main_menu():
     keyboard = [
-        [KeyboardButton(text="📋 Часті питання")],
-        [KeyboardButton(text="🍏 Категорії продуктів")],
-        [KeyboardButton(text="📅 Записатися на консультацію")]
+        [KeyboardButton(text="🍎 Категорії продуктів")],
+        [KeyboardButton(text="🥦 Продукти (пошук)")],
+        [KeyboardButton(text="📅 Записатись на консультацію")]
+    ]
+    return ReplyKeyboardMarkup(keyboard=keyboard, resize_keyboard=True)
+
+def get_product_categories_keyboard():
+    keyboard = [
+        [KeyboardButton(text="🍞 Хлібобулочні вироби"), KeyboardButton(text="🥦 Овочі")],
+        [KeyboardButton(text="🍓 Фрукти"), KeyboardButton(text="🥛 Молочні, безлактозні продукти")],
+        [KeyboardButton(text="🥜 Бобові, горіхи, тофу"), KeyboardButton(text="🍹 Напої")],
+        [KeyboardButton(text="🥩 М'ясо, риба, яйця"), KeyboardButton(text="🧈 Жири та масла")],
+        [KeyboardButton(text="🧂 Спеції, соуси"), KeyboardButton(text="🍪 Снеки, батончики, печиво")],
+        [KeyboardButton(text="🍰 Кондитерські вироби, цукор")],
+        [KeyboardButton(text="🔙 Назад до головного меню")]
     ]
     return ReplyKeyboardMarkup(keyboard=keyboard, resize_keyboard=True)
