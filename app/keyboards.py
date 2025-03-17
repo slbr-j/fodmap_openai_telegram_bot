54f2eecaa1425d05455be1c2cded706a61c28cf9
slbr-j <104697639+slbr-j@users.noreply.github.com>
2025-03-17 15:53:56 +0100

    Update keyboards.py

--- app/keyboards.py
+++ app/keyboards.py
@@ -1,12 +1,9 @@
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 def get_main_keyboard():
-    keyboard = InlineKeyboardMarkup(
-        inline_keyboard=[
-            [InlineKeyboardButton(text="✅ Консультація", callback_data="consult")],
-            [InlineKeyboardButton(text="📄 Поради", callback_data="tips")],
-            [InlineKeyboardButton(text="❓ FAQ", callback_data="faq")],
-        ]
-    )
-    return keyboard
+    keyboard = [
+        [KeyboardButton(text="📋 Часті питання")],
+        [KeyboardButton(text="🍏 Категорії продуктів")],
+        [KeyboardButton(text="📅 Записатися на консультацію")]
+    ]
+    return ReplyKeyboardMarkup(keyboard=keyboard, resize_keyboard=True)
