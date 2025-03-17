557e8c92131f242d6fa6dfeba26026f79541859e
slbr-j <104697639+slbr-j@users.noreply.github.com>
2025-03-17 22:45:33 +0100

    Update keyboards.py

--- app/keyboards.py
+++ app/keyboards.py
@@ -1,5 +1,5 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
-from data_loader import CATEGORIES
+from app.data_loader import CATEGORIES
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 def get_main_menu():
